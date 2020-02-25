78bc57bbbbd3bd5e35f9fafa63f14b837902b81d
Thomas Gaston <thomasgaston11@gmail.com>
2020-02-25 15:14:09 -0700

    all CRUD actions completed, login finished, create new destinations finished

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,15 +1,53 @@
 class DestinationsController < ApplicationController
-
+  before_action :set_destination, only: [:show, :edit, :update, :destroy]
 
   def index
+    @destinations = current_user.destinations
   end
 
   def show
   end
 
   def new
+    @destination = Destination.new
+  end
+
+  def create
+    @destination = current_user.destinations.new(destination_params)
+    if @destination.save
+      flash[:success] = "New Destination Created"
+      redirect_to destinations_path
+    else
+      flash[:error] = "Error #{@destination.errors.full_messages.join('\n')}"
+      render :new
+    end
+  end
+
+  def update
+    if @destination.update(destination_params)
+      redirect_to destinations_path
+    else
+      render :edit
+    end
   end
 
   def edit
   end
+
+  def destroy
+    @destination.destroy
+    redirect_to destinations_path
+  end
+
+
+  private
+
+  def set_destination
+    @destination = current_user.destinations.find(params[:id])
+  end
+
+  def destination_params
+    params.require(:destination).permit(:name, :location)
+  end
+  
 end
